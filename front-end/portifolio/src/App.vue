ffb4296d6f0273f9bd3dc857932ecb1fce4f0593
Wesley Ferreira <37143147+WesleyIsFerreira@users.noreply.github.com>
2021-07-26 22:06:04 -0300

    Criando projeto
    Instalando Tailwind css no Vue 3 com Vite

---
+++ front-end/portifolio/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+      <div class="md:flex">
+        <div class="md:flex-shrink-0">
+          <img class="transform rotate-45 h-48 w-full object-cover md:h-full md:w-48" src="https://image.api.playstation.com/cdn/UP0002/CUSA07402_00/03ZtrPdjasIxzi8QrzOb2zCIHLMydFbh.png" alt="Man looking at item at a store">
+        </div>
+        <div class="p-8">
+          <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
+          <a href="" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding customers for your new business</a>
+          <p class="mt-2 text-gray-500">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
--- portifolio/src/main.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { VuesticPlugin } from 'vuestic-ui' 
-import 'vuestic-ui/dist/vuestic-ui.css' 
-createApp(App).use(VuesticPlugin) .mount('#app')
