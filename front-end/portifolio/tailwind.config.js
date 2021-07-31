785166b161b1f37ac862fc1156280a294550fbe8
BrunoPoiano <36886801+BrunoPoiano@users.noreply.github.com>
2021-07-31 13:31:35 -0300

    retirando rotate

--- front-end/portifolio/tailwind.config.js
+++ front-end/portifolio/tailwind.config.js
@@ -3,18 +3,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
-    rotate: {
-      "-180": "-180deg",
-      "-135": "-135deg",
-      "-90": "-90deg",
-      "-45": "-45deg",
-      0: "0",
-      45: "45deg",
-      90: "90deg",
-      135: "135deg",
-      180: "180deg",
-      270: "270deg",
-    },
+
   },
   variants: {
     extend: {},
